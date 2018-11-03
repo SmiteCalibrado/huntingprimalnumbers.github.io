70f66ee94f4c9f1a4506b6f0b3c40c2ae26170d4
SmiteCalibrado <herogamer28@gmail.com>
2018-11-02 23:40:22 -0300

    CapitalismoXSocialismo

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1541212149,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ceu.png",
+		"images/grama-sheet0.png",
+		"images/chao-sheet0.png",
+		"images/solido-sheet0.png",
+		"images/predios-sheet0.png",
+		"images/arvores.png",
+		"images/sprite-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/inimigo01-sheet0.png",
+		"images/sprite5-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
